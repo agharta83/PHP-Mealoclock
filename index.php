1d64a20973382d8ee925755bbe56a466e83dfece
agharta83 <35310694+agharta83@users.noreply.github.com>
2019-09-05 10:56:23 +0200

    create controllers and models

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php ?>
+<?php
+
+// FrontController
+$app = new Application();
+
+// Liste de "require"
+// Liste de routes
+$app->initRoutes();
+
+// Matching des routes ($match)
+$app->matching();
---
+++ src/Controllers/MainController.php
@@ -0,0 +1,7 @@
+<?php
+
+class MainController extends CoreController {
+    public function home() {
+        
+    }
+}
